ba96c04a9a912c7e75e6416d8cbecb93ec850bb0
felipe barrio <felipebariof@gmail.com>
2025-09-26 19:57:31 -0300

    entregando atividade

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("digite seu primeiro nome: ");
+nome = Console.ReadLine();
+Console.Write("digite seu sobrenome: ");
+sobrenome = Console.ReadLine();
+Console.Write("nome completo: ");
+Console.WriteLine($"{nome} {sobrenome}");
+Console.Write("nome de catálogo: ");
+Console.WriteLine($"{sobrenome.ToUpper()} {nome}");
